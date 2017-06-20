ca8fcf3818c06bb9cbe3d077c78fdd8c5b4934ba
Won Maung Thein <wonmaungthein@gmail.com>
2017-06-20 20:05:56 +0100

    Added JS file and selected the relevant buttons

---
+++ main.js
@@ -0,0 +1,23 @@
+var SHnews = document.querySelector('.shnews');
+SHnews.addEventListener('click',ShowHideNews);
+
+function ShowHideNews() {
+
+
+}
+
+
+
+var nameButton = document.querySelector('.nameInput');
+var emailButton = document.querySelector('.emailInput');
+var numberButton = document.querySelector('.numberInput');
+var submitButton = document.querySelector('.submitBtn');
+
+var MInfoButton = document.querySelector('.MInfo');
+MInfoButton.addEventListener('click',MoreInFo);
+
+function MoreInFo() {
+
+
+}
+
