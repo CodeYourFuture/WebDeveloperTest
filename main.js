7d028c2fee30b44ed1861fe2d5365788c06eca93
Won Maung Thein <wonmaungthein@gmail.com>
2017-06-21 15:16:46 +0100

    finished homework, fixed mistakes and done

--- main.js
+++ main.js
@@ -1,23 +1,83 @@
+
 var SHnews = document.querySelector('.shnews');
-SHnews.addEventListener('click',ShowHideNews);
+SHnews.addEventListener('click', ShowHideNews);
 
 function ShowHideNews() {
-
-
+   ShowHide(document.querySelector('#News'));
 }
 
+function ShowHide(element) {
+   if (element.className === "hidden") {
+       element.className = 'show';
+   } else {
+       element.className = 'hidden';
+   }
+}
 
-
-var nameButton = document.querySelector('.nameInput');
-var emailButton = document.querySelector('.emailInput');
-var numberButton = document.querySelector('.numberInput');
-var submitButton = document.querySelector('.submitBtn');
 
 var MInfoButton = document.querySelector('.MInfo');
-MInfoButton.addEventListener('click',MoreInFo);
+MInfoButton.addEventListener('click', MoreInFo);
 
 function MoreInFo() {
+   ShowHide(document.querySelector('#moreInfo p'));
 
 
 }
 
+var submitButton = document.querySelector('.submitBtn');
+
+submitButton.addEventListener('click', submitForm);
+
+function submitForm(event) {
+    event.preventDefault();
+
+    var nameInput = document.querySelector('#example-name-input');
+    var nameInputWV = nameInput.value;
+    var emailInput = document.querySelector('#example-email-input');
+    var emailInputWV = emailInput.value;
+    var numberInput = document.querySelector('#example-phone-input');
+    var numberInputWV = numberInput.value;
+
+    var SubmitFormIsValid = true;
+    if (nameInputWV.length === 0) {
+        nameInput.className = 'nameInput inValid';
+        SubmitFormIsValid = false;
+    } 
+    // else {
+    //     nameInput.className = 'nameInput';
+    //     nameInput.value = "";
+        
+    // }
+
+    if (emailInputWV.length === 0 || emailInputWV.indexOf('@') === -1) {
+        emailInput.className = 'emailInput inValid';
+        SubmitFormIsValid = false;
+    } 
+    // else {
+    //     emailInput.className = 'emailInput';
+    //     emailInput.value = "";
+    // }
+    if (numberInputWV.length > 11) {
+        numberInput.className = 'numberInput inValid';
+        SubmitFormIsValid = false;
+    } 
+    // else { 
+    //     numberInput.className = 'numberInput';
+    //     numberInput.value = "";
+        
+    // }
+
+    if (SubmitFormIsValid) {
+        alert('Thanks for filling the form');
+        numberInput.className = 'numberInput';
+        numberInput.value = "";
+    
+        emailInput.className = 'emailInput';
+        emailInput.value = "";
+    
+        nameInput.className = 'nameInput';
+        nameInput.value = "";
+    }
+    
+
+}
