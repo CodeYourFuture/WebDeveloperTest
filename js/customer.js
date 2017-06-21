1029c3bbe0986d0e3985ca0134842051560059f3
MrDwina <diwina1993@hotmail.com>
2017-06-21 17:54:36 +0000

    edit form validation to give alert massage if success or input fiel it will become red

--- js/customer.js
+++ js/customer.js
@@ -14,25 +14,96 @@ window.addEventListener('load', function () {
     submitForm.addEventListener('click', function (event) {
         event.preventDefault();
         const name = document.getElementById('name');
-        if (name.value === "") {
-            name.style.background = '#f00';
-        }else{
-            alert('Thank you for register with us');
-            return true;
-        }
         const email = document.getElementById('email');
-        if (email.value === "@" && email.value === "." && email.indexOf("@") !== -1 && email.indexOf(".") !== -1) {
-            alert('Thank you for register with us');
-            return true;
-        }else{
-            email.style.background = '#f00';
-        }
         const phoneNumber = document.getElementById('phoneNumber');
-        if (phoneNumber.value === "" ||  phoneNumber.value.length > 15 || phoneNumber.value.length < 11) {
-            phoneNumber.style.background = '#f00';
-        }else{
+
+        if (name.value !== "" && email.value.indexOf("@") !== 0 && email.value.indexOf("@") !== -1 && email.value.indexOf(".") !== -1 && email.value.lastIndexOf("@") < email.value.lastIndexOf(".") && email.value.lastIndexOf(".") < (email.value.length - 2) && phoneNumber.value !== "" && phoneNumber.value.length < 12) {
             alert('Thank you for register with us');
-            return true;
-        }
+        }else if (name.value !== "" && email.value.indexOf("@") !== 0 && email.value.indexOf("@") !== -1 && email.value.indexOf(".") !== -1 && email.value.lastIndexOf("@") < email.value.lastIndexOf(".") && email.value.lastIndexOf(".") < (email.value.length - 2)) {
+            if (name.value !== "" && email.value.indexOf("@") !== 0 && email.value.indexOf("@") !== -1 && email.value.indexOf(".") !== -1 && email.value.lastIndexOf("@") < email.value.lastIndexOf(".") && email.value.lastIndexOf(".") < (email.value.length - 2)) {
+                phoneNumber.style.background = '#f00';
+            }else if (name.value !== "" || email.value.indexOf("@") !== 0 && email.value.indexOf("@") !== -1 && email.value.indexOf(".") !== -1 && email.value.lastIndexOf("@") < email.value.lastIndexOf(".") && email.value.lastIndexOf(".") < (email.value.length - 2)) {
+                if (name.value !== "") {
+                    email.style.background = '#f00';
+                    phoneNumber.style.background = '#f00';
+                }else if (email.value.indexOf("@") !== 0 && email.value.indexOf("@") !== -1 && email.value.indexOf(".") !== -1 && email.value.lastIndexOf("@") < email.value.lastIndexOf(".") && email.value.lastIndexOf(".") < (email.value.length - 2)){
+                    name.style.background = '#f00';
+                    phoneNumber.style.background = '#f00';
+                }else{
+                    alert('error');
+                }
+            }
+        }else if (name.value !== "" && phoneNumber.value !== "" && phoneNumber.value.length < 12) {
+            if (name.value !== "" && phoneNumber.value !== "" && phoneNumber.value.length < 12) {
+                email.style.background = '#f00';
+            }else if (name.value !== "" || phoneNumber.value !== "" && phoneNumber.value.length < 12) {
+                if (name.value !== "") {
+                    email.style.background = '#f00';
+                    phoneNumber.style.background = '#f00';
+                }else if (phoneNumber.value !== "" && phoneNumber.value.length < 12){
+                    name.style.background = '#f00';
+                    email.style.background = '#f00';
+                }else{
+                    alert('error');
+                }
+            }
+        }else if (email.value.indexOf("@") !== 0 && email.value.indexOf("@") !== -1 && email.value.indexOf(".") !== -1 && email.value.lastIndexOf("@") < email.value.lastIndexOf(".") && email.value.lastIndexOf(".") < (email.value.length - 2) && phoneNumber.value !== "" && phoneNumber.value.length < 12){
+            if (email.value.indexOf("@") !== 0 && email.value.indexOf("@") !== -1 && email.value.indexOf(".") !== -1 && email.value.lastIndexOf("@") < email.value.lastIndexOf(".") && email.value.lastIndexOf(".") < (email.value.length - 2) && phoneNumber.value !== "" && phoneNumber.value.length < 12) {
+                name.style.background = '#f00';
+            }else if (email.value.indexOf("@") !== 0 && email.value.indexOf("@") !== -1 && email.value.indexOf(".") !== -1 && email.value.lastIndexOf("@") < email.value.lastIndexOf(".") && email.value.lastIndexOf(".") < (email.value.length - 2) || phoneNumber.value !== "" && phoneNumber.value.length < 12) {
+                if (email.value.indexOf("@") !== 0 && email.value.indexOf("@") !== -1 && email.value.indexOf(".") !== -1 && email.value.lastIndexOf("@") < email.value.lastIndexOf(".") && email.value.lastIndexOf(".") < (email.value.length - 2)) {
+                    name.style.background = '#f00';
+                    phoneNumber.style.background = '#f00';
+                }else if (phoneNumber.value !== "" && phoneNumber.value.length < 12){
+                    name.style.background = '#f00';
+                    email.style.background = '#f00';
+                }else{
+                    alert('error');
+                }
+            }
+        }else if(name.value === "" && email.value.indexOf("@") === 0 || email.value.indexOf("@") === -1 || email.value.indexOf(".") === -1 || email.value.lastIndexOf("@") > email.value.lastIndexOf(".") || email.value.lastIndexOf(".") > (email.value.length - 2) && phoneNumber.value === "" || phoneNumber.value.length > 11) {
+            name.style.background = '#f00';
+            email.style.background = '#f00';
+            phoneNumber.style.background = '#f00';
+        }else if (name.value === "" && email.value.indexOf("@") === 0 || email.value.indexOf("@") === -1 || email.value.indexOf(".") === -1 || email.value.lastIndexOf("@") > email.value.lastIndexOf(".") || email.value.lastIndexOf(".") > (email.value.length - 2)) {
+            if (name.value === "" && email.value.indexOf("@") === 0 || email.value.indexOf("@") === -1 || email.value.indexOf(".") === -1 || email.value.lastIndexOf("@") > email.value.lastIndexOf(".") || email.value.lastIndexOf(".") > (email.value.length - 2)) {
+                name.style.background = '#f00';
+                email.style.background = '#f00';
+            }else if (name.value === "" || email.value.indexOf("@") === 0 || email.value.indexOf("@") === -1 || email.value.indexOf(".") === -1 || email.value.lastIndexOf("@") > email.value.lastIndexOf(".") || email.value.lastIndexOf(".") > (email.value.length - 2)) {
+                if (name.value === "") {
+                    name.style.background = '#f00';
+                }else if (email.value.indexOf("@") === 0 || email.value.indexOf("@") === -1 || email.value.indexOf(".") === -1 || email.value.lastIndexOf("@") > email.value.lastIndexOf(".") || email.value.lastIndexOf(".") > (email.value.length - 2)){
+                    email.style.background = '#f00';
+                }else{
+                    alert('error');
+                }
+            }
+        }else if (name.value === "" && phoneNumber.value === "" || phoneNumber.value.length > 11) {
+            if (name.value === "" && phoneNumber.value === "" || phoneNumber.value.length > 11) {
+                name.style.background = '#f00';
+                phoneNumber.style.background = '#f00';
+            }else if (name.value === "" && phoneNumber.value === "" || phoneNumber.value.length > 11) {
+                if (name.value === "") {
+                    name.style.background = '#f00';
+                }else if (phoneNumber.value === "" || phoneNumber.value.length > 11){
+                    phoneNumber.style.background = '#f00';
+                }else{
+                    alert('error');
+                }
+            }
+        }else if (email.value.indexOf("@") === 0 || email.value.indexOf("@") === -1 || email.value.indexOf(".") === -1 || email.value.lastIndexOf("@") > email.value.lastIndexOf(".") || email.value.lastIndexOf(".") > (email.value.length - 2) && phoneNumber.value === "" || phoneNumber.value.length > 11){
+            if (email.value.indexOf("@") === 0 || email.value.indexOf("@") === -1 || email.value.indexOf(".") === -1 || email.value.lastIndexOf("@") > email.value.lastIndexOf(".") || email.value.lastIndexOf(".") > (email.value.length - 2) && phoneNumber.value === "" || phoneNumber.value.length > 11) {
+                email.style.background = '#f00';
+                phoneNumber.style.background = '#f00';
+            }else if (email.value.indexOf("@") === 0 || email.value.indexOf("@") === -1 || email.value.indexOf(".") === -1 || email.value.lastIndexOf("@") > email.value.lastIndexOf(".") || email.value.lastIndexOf(".") > (email.value.length - 2) || phoneNumber.value === "" || phoneNumber.value.length > 11) {
+                if (email.value.indexOf("@") === 0 || email.value.indexOf("@") === -1 || email.value.indexOf(".") === -1 || email.value.lastIndexOf("@") > email.value.lastIndexOf(".") || email.value.lastIndexOf(".") > (email.value.length - 2)) {
+                    nemailame.style.background = '#f00';
+                }else if (phoneNumber.value === "" || phoneNumber.value.length){
+                    phoneNumber.style.background = '#f00';
+                }else{
+                    alert('error');
+                }
+            }
+        }          
     });
 },false);
