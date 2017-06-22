7c74e7dd802799cc0403c5eb75e891bae42694fe
Anthonythuo <antonykariuki@yahoo.com>
2017-06-22 18:34:14 +0100

    My current changes to my WebDevelopertest

---
+++ event.js
@@ -0,0 +1,8 @@
+var button =document.querySelector('#newsButton');
+button.addEventListener('click',doSomething);
+
+function doSomething (){
+  //if my <p>is showing hide it,else show it
+var news = document.querySelector('#newsInfo');
+
+}
